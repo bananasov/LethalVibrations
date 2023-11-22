7defbceda1e36ebad4f7a9f5b8b1eec4dd9eb56d
sovnya <enp7s1@tutanota.com>
2023-11-22 20:40:43 +0100

    Actually make damage dealt config useful

---
+++ LethalVibrations/Patches/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using LethalVibrations.Buttplug;
+
+namespace LethalVibrations.Patches
+{
+    internal class EnemyAI
+    {
+        private static ManualLogSource Logger { get; set; }
+
+        public static void Init(ManualLogSource logger)
+        {
+            Logger = logger;
+        }
+
+        [HarmonyPatch(typeof(EnemyAI), "HitEnemyOnLocalClient")]
+        [HarmonyPostfix]
+        static void HitEnemyOnLocalClientPatch()
+        {
+            Logger.LogInfo($"HitEnemyOnLocalClient got called");
+
+            if (Plugin.DeviceManager.IsConnected() && Config.VibrateDamageDealtEnabled.Value)
+            {
+                Plugin.DeviceManager.VibrateConnectedDevices(0.5f + Config.VibrateDamageDealtAmplifier.Value, Config.VibrateDamageDealtTime.Value);
+            }
+        }
+    }
+}
