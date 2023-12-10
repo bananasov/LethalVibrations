8dd66a65fa9b3252fb6aad5aae78241b3729764c
sovnya <enp7s1@tutanota.com>
2023-12-10 13:55:42 +0100

    feat: Add Ping scan vibration

--- LethalVibrations/Patches/HUDManager.cs
+++ LethalVibrations/Patches/HUDManager.cs
@@ -24,18 +24,30 @@ namespace LethalVibrations.Patches
                 switch (shakeType)
             	{
             		case ScreenShakeType.Small:
-            			Plugin.DeviceManager.VibrateConnectedDevices(0.3f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenTime.Value);
+            			Plugin.DeviceManager.VibrateConnectedDevices(0.3f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenDuration.Value);
             			return;
             		case ScreenShakeType.Big:
-                        Plugin.DeviceManager.VibrateConnectedDevices(0.6f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenTime.Value);
+                        Plugin.DeviceManager.VibrateConnectedDevices(0.6f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenDuration.Value);
             			return;
             		case ScreenShakeType.Long:
-                        Plugin.DeviceManager.VibrateConnectedDevices(0.5f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenTime.Value + 0.6f);
+                        Plugin.DeviceManager.VibrateConnectedDevices(0.5f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenDuration.Value + 0.6f);
             			return;
             		case ScreenShakeType.VeryStrong:
-            			Plugin.DeviceManager.VibrateConnectedDevices(0.9f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenTime.Value + 0.3f);
+            			Plugin.DeviceManager.VibrateConnectedDevices(0.9f + Config.VibrateShakeScreenAmplifier.Value, Config.VibrateShakeScreenDuration.Value + 0.3f);
             			return;
             	}
+            }
+        }
+
+        [HarmonyPatch(typeof(HUDManager), "PingScan_performed")]
+        [HarmonyPostfix]
+        static void PingScan_performedPatch()
+        {
+            Logger.LogInfo($"PingScan_performed got called");
+
+            if (Plugin.DeviceManager.IsConnected() && Config.VibrateShakeScreenEnabled.Value)
+            {
+                Plugin.DeviceManager.VibrateConnectedDevices(0.3f + Config.PingScanAmplifier.Value, Config.PingScanDuration.Value);
             }
         }
     }
