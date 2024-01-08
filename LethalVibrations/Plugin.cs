694e58572104c8552ce268568a657bfe87ed77ff
sovnya <enp7s1@tutanota.com>
2024-01-08 15:05:02 +0100

    fix: Fix the stupid BepInEx annotations

--- LethalVibrations/Plugin.cs
+++ LethalVibrations/Plugin.cs
@@ -5,9 +5,14 @@ using LethalVibrations.Buttplug;
 
 namespace LethalVibrations
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        // Fuck BepInEx
+        private const string PLUGIN_GUID = "github.bananasov.LethalVibrations";
+        private const string PLUGIN_NAME = "LethalVibrations";
+        private const string PLUGIN_VERSION = "0.0.5";
+        
         internal static DeviceManager DeviceManager { get; private set; }
         internal static ManualLogSource Mls { get; private set; }
         
@@ -18,14 +23,14 @@ namespace LethalVibrations
             DeviceManager = new DeviceManager("LethalVibrations");
             DeviceManager.ConnectDevices();
 
-            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            var harmony = new Harmony(PLUGIN_GUID);
             harmony.PatchAll(typeof(Patches.PlayerControllerBPatches));
             harmony.PatchAll(typeof(Patches.ItemChargerPatches));
             harmony.PatchAll(typeof(Patches.EnemyAIPatches));
             harmony.PatchAll(typeof(Patches.HUDManagerPatches));
             harmony.PatchAll(typeof(Patches.GrabbableObjectPatches));
 
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} ({PluginInfo.PLUGIN_VERSION}) is loaded!");
+            Logger.LogInfo($"Plugin {PLUGIN_NAME} ({PLUGIN_VERSION}) is loaded!");
         }
     }
 }
