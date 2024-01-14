5967d2939c7022d2c68ab19dd13f385baccf4d01
sovnya <enp7s1@tutanota.com>
2024-01-14 20:28:03 +0100

    fix: Let other clients be able to vibrate your plug when the ship horn is pulled
    :3

--- LethalVibrations/Patches/ShipAlarmCord.cs
+++ LethalVibrations/Patches/ShipAlarmCord.cs
@@ -1,10 +1,47 @@
-﻿using HarmonyLib;
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
 using LethalVibrations.Buttplug;
 
 namespace LethalVibrations.Patches
 {
     public class ShipAlarmCordPatches
     {
+        [HarmonyPatch(typeof(ShipAlarmCord), "PullCordClientRpc")]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> PullCordClientRpcTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var newInstructions = new List<CodeInstruction>(instructions);
+
+            var index = newInstructions.FindLastIndex(i => i.opcode == OpCodes.Nop) + 1;
+            
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ShipAlarmCordPatches), nameof(ShipAlarmCordPatches.StartVibrating))),
+            });
+
+            foreach (var t in newInstructions)
+                yield return t;
+        }
+        
+        [HarmonyPatch(typeof(ShipAlarmCord), "StopPullingCordClientRpc")]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> StopPullingCordClientRpcTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var newInstructions = new List<CodeInstruction>(instructions);
+
+            var index = newInstructions.FindLastIndex(i => i.opcode == OpCodes.Nop) + 1;
+            
+            newInstructions.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ShipAlarmCordPatches), nameof(ShipAlarmCordPatches.StopVibrating))),
+            });
+
+            foreach (var t in newInstructions)
+                yield return t;
+        }
+
+        #region This part makes the vibrations work on the client
         [HarmonyPatch(typeof(ShipAlarmCord), "HoldCordDown")]
         [HarmonyPostfix]
         private static void HoldCordDownPatch()
@@ -12,10 +49,7 @@ namespace LethalVibrations.Patches
             Plugin.Mls.LogDebug($"ShipAlarmCord.HoldCordDown got called");
             
             // I am well aware that `HoldCordDown` gets called SO MANY FUCKING TIMES.
-            if (Plugin.DeviceManager.IsConnected() && Config.ShipHornEnabled.Value)
-            {
-                Plugin.DeviceManager.VibrateConnectedDevices(Config.ShipHornStrength.Value);
-            }
+            StartVibrating();
         }
         
         [HarmonyPatch(typeof(ShipAlarmCord), "StopHorn")]
@@ -24,10 +58,26 @@ namespace LethalVibrations.Patches
         {
             Plugin.Mls.LogDebug($"ShipAlarmCord.StopHorn got called");
             
+            StopVibrating();
+        }
+        #endregion
+
+        #region Helper functions so the IL transpiler can call stuff :3
+        private static void StartVibrating()
+        {
+            if (Plugin.DeviceManager.IsConnected() && Config.ShipHornEnabled.Value)
+            {
+                Plugin.DeviceManager.VibrateConnectedDevices(Config.ShipHornStrength.Value);
+            }
+        }
+        
+        private static void StopVibrating()
+        {
             if (Plugin.DeviceManager.IsConnected() && Config.ShipHornEnabled.Value)
             {
                 Plugin.DeviceManager.StopConnectedDevices();
             }
         }
+        #endregion
     }
 }
