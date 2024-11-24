27df9b3f0f8816b500a333d26402f21a050699ea
sovnya <enp7s1@tutanota.com>
2024-11-24 14:00:32 +0100

    feat: Add JesterAI hooks for future features
    Most notably, stroker oscillation.

---
+++ src/LethalHaptics/Hooks/JesterAIHooks.cs
@@ -0,0 +1,33 @@
+﻿using LethalHaptics.Utils;
+using UnityEngine;
+
+namespace LethalHaptics.Hooks;
+
+// ReSharper disable once UnusedType.Global
+public class JesterAIHooks
+{
+    private static readonly int TurningCrank = Animator.StringToHash("turningCrank");
+    private static readonly int PoppedOut = Animator.StringToHash("poppedOut");
+
+    [ButtplugPatch]
+    public static void Initialize()
+    {
+        On.JesterAI.Update += JesterAIOnUpdate;
+    }
+
+    private static void JesterAIOnUpdate(On.JesterAI.orig_Update orig, JesterAI self)
+    {
+        orig(self);
+        
+        // NOTE: I am not sure if there could be two jesters that spawn on a single map.
+        if (self.creatureAnimator.GetBool(TurningCrank))
+        {
+            // TODO: Stroker oscillation 
+        }
+
+        if (self.creatureAnimator.GetBool(PoppedOut))
+        {
+            // TODO: Constant vibration when jester is popped up   
+        }
+    }
+}
